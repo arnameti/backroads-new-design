f9e7685104f59e71adbd27d0abc5d03d96b2ea35
arnameti <arnameti@gmail.com>
2021-11-18 09:58:08 +0100

    navbar - toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-list");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+
